e7ed280f8d27bcc13115fe4e96a10a9517045ece
Christopher Head <chead@chead.ca>
2017-02-26 22:41:35 -0800

    Test code to print events.

---
+++ iecc8/DispatcherCallbacks.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Iecc8 {
+	class DispatcherCallbacks : IDispatcher {
+		public void DTMF(Messages.DTMFMessage msg) {
+			Console.WriteLine("< DTMF(" + msg + ")");
+		}
+
+		public void PermissionUpdate(Messages.DispatcherPermissionMessage msg) {
+			Console.WriteLine("< PermissionUpdate(" + msg + ")");
+		}
+
+		public void Ping() {
+			Console.WriteLine("< Ping()");
+		}
+
+		public void RadioText(Messages.RadioTextMessage msg) {
+			Console.WriteLine("< RadioTextMessage(" + msg + ")");
+		}
+
+		public void SendSimulationState(Messages.SimulationStateMessage msg) {
+			Console.WriteLine("< SendSimulationState(" + msg + ")");
+		}
+
+		public void SetInterlockErrorSwitches(Messages.InterlockErrorSwitchesMessage msg) {
+			Console.WriteLine("< SetInterlockErrorSwitches(" + msg + ")");
+		}
+
+		public void SetOccupiedBlocks(Messages.OccupiedBlocksMessage msg) {
+			Console.WriteLine("< SetOccupiedBlocks(" + msg + ")");
+		}
+
+		public void SetOccupiedSwitches(Messages.OccupiedSwitchesMessage msg) {
+			Console.WriteLine("< SetOccupiedSwitches(" + msg + ")");
+		}
+
+		public void SetReversedSwitches(Messages.ReversedSwitchesMessage msg) {
+			Console.WriteLine("< SetReversedSwitches(" + msg + ")");
+		}
+
+		public void SetSignals(Messages.SignalsMessage msg) {
+			Console.WriteLine("< SetSignals(" + msg + ")");
+		}
+
+		public void SetUnlockedSwitches(Messages.UnlockedSwitchesMessage msg) {
+			Console.WriteLine("< SetUnlockedSwitches(" + msg + ")");
+		}
+
+		public void UpdateTrainData(Messages.TrainDataMessage msg) {
+			Console.WriteLine("< UpdateTrainData(" + msg + ")");
+		}
+	}
+}
--- iecc8/Program.cs
+++ iecc8/Program.cs
@@ -1,8 +1,25 @@
-﻿using System;
+﻿using System.ServiceModel;
+using System.Threading.Tasks;
 
 namespace Iecc8 {
-	class Program {
-		static void Main(string[] args) {
+	public static class Program {
+		private const string ADDRESS = "net.tcp://localhost:15192/Run8";
+
+		public static void Main(string[] args) {
+			Task.Run(Run).Wait();
+		}
+
+		private static async Task Run() {
+			NetTcpBinding binding = new NetTcpBinding(SecurityMode.None);
+			DispatcherCallbacks cbs = new DispatcherCallbacks();
+			InstanceContext ic = new InstanceContext(cbs);
+			using (DuplexChannelFactory<IRun8> factory = new DuplexChannelFactory<IRun8>(ic, binding, ADDRESS)) {
+				Run8Wrapper obj = new Run8Wrapper(factory.CreateChannel());
+				await obj.DispatcherConnectedAsync();
+				for (;;) {
+					System.Threading.Thread.Sleep(5000);
+				}
+			}
 		}
 	}
 }
