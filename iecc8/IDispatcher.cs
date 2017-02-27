74fe66c885e859915258a7b3f4d2479a00aebffe
Christopher Head <chead@chead.ca>
2017-02-26 22:41:18 -0800

    Add the interfaces that Run8 and ED implement.

---
+++ iecc8/IDispatcher.cs
@@ -0,0 +1,97 @@
+﻿using System.ServiceModel;
+
+namespace Iecc8 {
+	/// <summary>
+	/// The API exposed by the dispatcher, providing callbacks that Run8 invokes to notify the dispatcher of state changes in the world.
+	/// </summary>
+	public interface IDispatcher {
+		/// <summary>
+		/// Notifies of a radio tone received by a tower.
+		/// </summary>
+		/// <param name="pMessage">Details of the tone.</param>
+		[OperationContract(IsOneWay = true)]
+		void DTMF(Messages.DTMFMessage pMessage);
+
+		/// <summary>
+		/// Notifies of a change in the dispatcher client's access level.
+		/// </summary>
+		/// <param name="pMessage">Details of the access level change.</param>
+		[OperationContract(IsOneWay = true)]
+		void PermissionUpdate(Messages.DispatcherPermissionMessage pMessage);
+
+		/// <summary>
+		/// Does nothing.
+		/// </summary>
+		[OperationContract(IsOneWay = true)]
+		void Ping();
+
+		/// <summary>
+		/// Notifies of a text message sent to a radio channel by a player.
+		/// </summary>
+		/// <param name="pMessage">Details of the radio message.</param>
+		[OperationContract(IsOneWay = true)]
+		void RadioText(Messages.RadioTextMessage pMessage);
+
+		/// <summary>
+		/// Notifies of the current in-game date and time and whether the game is connected to a multiplayer host.
+		/// </summary>
+		/// <param name="pMessage">Details of the new situation.</param>
+		[OperationContract(IsOneWay = true)]
+		void SendSimulationState(Messages.SimulationStateMessage pMessage);
+
+		/// <summary>
+		/// Notifies of which power points have been hand-cranked to an inconsistent position.
+		/// </summary>
+		/// <remarks>
+		/// An example is a pair of points forming a crossover with one side cranked normal and the other cranked reverse.
+		/// </remarks>
+		/// <param name="pMessage">The list of inconsistent power points.</param>
+		[OperationContract(IsOneWay = true)]
+		void SetInterlockErrorSwitches(Messages.InterlockErrorSwitchesMessage pMessage);
+
+		/// <summary>
+		/// Notifies of which power points have trains over them.
+		/// </summary>
+		/// <param name="pMessage">The list of power points with trains over them.</param>
+		[OperationContract(IsOneWay = true)]
+		void SetOccupiedSwitches(Messages.OccupiedSwitchesMessage pMessage);
+
+		/// <summary>
+		/// Notifies of which track circuits have trains in them.
+		/// </summary>
+		/// <param name="pMessage">The list of occupied track circuits.</param>
+		[OperationContract(IsOneWay = true)]
+		void SetOccupiedBlocks(Messages.OccupiedBlocksMessage pMessage);
+
+		/// <summary>
+		/// Notifies of which power points are in the reverse position.
+		/// </summary>
+		/// <remarks>
+		/// This includes both points which are driven to the reverse position under signaller control and also unlocked points which are hand-cranked reverse.
+		/// </remarks>
+		/// <param name="pMessage">The list of reversed power points.</param>
+		[OperationContract(IsOneWay = true)]
+		void SetReversedSwitches(Messages.ReversedSwitchesMessage pMessage);
+
+		/// <summary>
+		/// Notifies of the state of signals.
+		/// </summary>
+		/// <param name="pMessage">The states of the signals in the area.</param>
+		[OperationContract(IsOneWay = true)]
+		void SetSignals(Messages.SignalsMessage pMessage);
+
+		/// <summary>
+		/// Notifies of which power points are unlocked for hand cranking.
+		/// </summary>
+		/// <param name="pMessage">The list of unlocked power points.</param>
+		[OperationContract(IsOneWay = true)]
+		void SetUnlockedSwitches(Messages.UnlockedSwitchesMessage pMessage);
+
+		/// <summary>
+		/// Provides information about a train in the area.
+		/// </summary>
+		/// <param name="pMessage">Details about one train.</param>
+		[OperationContract(IsOneWay = true)]
+		void UpdateTrainData(Messages.TrainDataMessage pMessage);
+	}
+}
---
+++ iecc8/IRun8.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Iecc8 {
+	/// <summary>
+	/// The API exposed by Run8, providing functions that the dispatcher uses to manipulate the world.
+	/// </summary>
+	[ServiceContract(CallbackContract = typeof(IDispatcher), Name = "IWCFRun8", SessionMode = SessionMode.Required)]
+	public interface IRun8 {
+		/// <summary>
+		/// Orders an AI driver to board a train and start driving.
+		/// </summary>
+		/// <param name="pMessage">Which train to drive.</param>
+		[OperationContract(AsyncPattern = true, IsOneWay = true)]
+		IAsyncResult BeginAIRecrewTrain(Messages.AIRecrewTrainMessage pMessage, AsyncCallback cb, object state);
+
+		void EndAIRecrewTrain(IAsyncResult result);
+
+		/// <summary>
+		/// Chooses whether a signal is on or off and, if off, whether it has automatic working enabled.
+		/// </summary>
+		/// <param name="pMessage">Details of the change to make.</param>
+		[OperationContract(AsyncPattern = true, IsOneWay = true)]
+		IAsyncResult BeginChangeSignal(Messages.DispatcherSignalMessage pMessage, AsyncCallback cb, object state);
+
+		void EndChangeSignal(IAsyncResult result);
+
+		/// <summary>
+		/// Notifies Run8 that a dispatcher is ready to work.
+		/// </summary>
+		[OperationContract(AsyncPattern = true, IsOneWay = true)]
+		IAsyncResult BeginDispatcherConnected(AsyncCallback cb, object state);
+
+		void EndDispatcherConnected(IAsyncResult result);
+
+		/// <summary>
+		/// Orders an AI to brake and hold its position or resume normal driving.
+		/// </summary>
+		/// <param name="pMessage">Details of the orders.</param>
+		[OperationContract(AsyncPattern = true, IsOneWay = true)]
+		IAsyncResult BeginHoldAITrain(Messages.HoldAITrainMessage pMessage, AsyncCallback cb, object state);
+
+		void EndHoldAITrain(IAsyncResult result);
+
+		/// <summary>
+		/// Does nothing.
+		/// </summary>
+		[OperationContract(AsyncPattern = true, IsOneWay = true)]
+		IAsyncResult BeginPing(AsyncCallback cb, object state);
+
+		void EndPing(IAsyncResult result);
+
+		/// <summary>
+		/// Sends a text message over a radio channel.
+		/// </summary>
+		/// <param name="pMessage">Details of the message to send.</param>
+		[OperationContract(AsyncPattern = true, IsOneWay = true)]
+		IAsyncResult BeginRadioText(Messages.RadioTextMessage pMessage, AsyncCallback cb, object state);
+
+		void EndRadioText(IAsyncResult result);
+
+		/// <summary>
+		/// Orders an AI driver to disembark from its train once stopped, or to remain in its seat.
+		/// </summary>
+		/// <param name="pMessage">Details of the orders.</param>
+		[OperationContract(AsyncPattern = true, IsOneWay = true)]
+		IAsyncResult BeginRelinquishAITrain(Messages.RelinquishAITrainMessage pMessage, AsyncCallback cb, object state);
+
+		void EndRelinquishAITrain(IAsyncResult result);
+
+		/// <summary>
+		/// Instantly stops all movement of an AI train orders the driver to hold position.
+		/// </summary>
+		/// <param name="pMessage">Which train to stop.</param>
+		[OperationContract(AsyncPattern = true, IsOneWay = true)]
+		IAsyncResult BeginStopAITrain(Messages.StopAITrainMessage pMessage, AsyncCallback cb, object state);
+
+		void EndStopAITrain(IAsyncResult result);
+
+		/// <summary>
+		/// Drives power points to a position, unlocks them for hand cranking, or relocks them for power operation.
+		/// </summary>
+		/// <param name="pMessage">Details of the change to make.</param>
+		[OperationContract(AsyncPattern = true, IsOneWay = true)]
+		IAsyncResult BeginThrowSwitch(Messages.DispatcherSwitchMessage pMessage, AsyncCallback cb, object state);
+
+		void EndThrowSwitch(IAsyncResult result);
+
+		/// <summary>
+		/// Teleports the player to power points.
+		/// </summary>
+		/// <param name="pMessage">Which points to teleport to.</param>
+		[OperationContract(AsyncPattern = true, IsOneWay = true)]
+		IAsyncResult BeginTransportPlayer(Messages.TransportPlayerMessage pMessage, AsyncCallback cb, object state);
+
+		void EndTransportPlayer(IAsyncResult result);
+
+		/// <summary>
+		/// Teleports the player to a track circuit.
+		/// </summary>
+		/// <param name="pMessage">Which track circuit to teleport to.</param>
+		[OperationContract(AsyncPattern = true, IsOneWay = true)]
+		IAsyncResult BeginTransportPlayerToBlock(Messages.TransportPlayerToBlockMessage pMessage, AsyncCallback cb, object state);
+
+		void EndTransportPlayerToBlock(IAsyncResult result);
+	}
+}
