be86e931656bc6e936c661e697d42052b4884cc4
Christopher Head <chead@chead.ca>
2017-03-01 23:07:27 -0800

    Show changes in objects rather than raw messages.

--- iecc8/DispatcherCallbacks.cs
+++ iecc8/DispatcherCallbacks.cs
@@ -1,53 +1,94 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Iecc8 {
 	class DispatcherCallbacks : IDispatcher {
+		private Dictionary<int, List<int>> lastOccupied = new Dictionary<int, List<int>>();
+		private Dictionary<int, List<int>> lastManual = new Dictionary<int, List<int>>();
+		private Dictionary<int, List<int>> lastReversedSwitches = new Dictionary<int, List<int>>();
+		private Dictionary<int, List<Messages.ESignalIndication>> lastSignals = new Dictionary<int, List<Messages.ESignalIndication>>();
+
 		public void DTMF(Messages.DTMFMessage msg) {
-			Console.WriteLine("< DTMF(" + msg + ")");
 		}
 
 		public void PermissionUpdate(Messages.DispatcherPermissionMessage msg) {
-			Console.WriteLine("< PermissionUpdate(" + msg + ")");
 		}
 
 		public void Ping() {
-			Console.WriteLine("< Ping()");
 		}
 
 		public void RadioText(Messages.RadioTextMessage msg) {
-			Console.WriteLine("< RadioTextMessage(" + msg + ")");
 		}
 
 		public void SendSimulationState(Messages.SimulationStateMessage msg) {
-			Console.WriteLine("< SendSimulationState(" + msg + ")");
 		}
 
 		public void SetInterlockErrorSwitches(Messages.InterlockErrorSwitchesMessage msg) {
-			Console.WriteLine("< SetInterlockErrorSwitches(" + msg + ")");
 		}
 
 		public void SetOccupiedBlocks(Messages.OccupiedBlocksMessage msg) {
-			Console.WriteLine("< SetOccupiedBlocks(" + msg + ")");
+			UpdateListOfIntegers(lastOccupied, msg.Route, msg.OccupiedBlocks, "block", "occupied", "cleared");
+			UpdateListOfIntegers(lastManual, msg.Route, msg.OpenManualSwitchBlocks, "block hand points", "reversed", "normalized");
 		}
 
 		public void SetOccupiedSwitches(Messages.OccupiedSwitchesMessage msg) {
-			Console.WriteLine("< SetOccupiedSwitches(" + msg + ")");
 		}
 
 		public void SetReversedSwitches(Messages.ReversedSwitchesMessage msg) {
-			Console.WriteLine("< SetReversedSwitches(" + msg + ")");
+			UpdateListOfIntegers(lastReversedSwitches, msg.Route, msg.ReversedSwitches, "switch", "reversed", "normalized");
 		}
 
 		public void SetSignals(Messages.SignalsMessage msg) {
-			Console.WriteLine("< SetSignals(" + msg + ")");
+			if (lastSignals.ContainsKey(msg.Route)) {
+				List<Messages.ESignalIndication> old = lastSignals[msg.Route];
+				int max = Math.Max(old.Count, msg.Signals.Count);
+				for (int i = 0; i != max; ++i) {
+					if (i >= old.Count) {
+						Console.WriteLine("Route " + msg.Route + " signal number " + i + " added when it used not to exist.");
+					} else if (i >= msg.Signals.Count) {
+						Console.WriteLine("Route " + msg.Route + " signal number " + i + " disappeared.");
+					} else if (msg.Signals[i] != old[i]) {
+						Console.WriteLine("Route " + msg.Route + " signal number " + i + " changed from " + old[i] + " to " + msg.Signals[i] + ".");
+					}
+				}
+				lastSignals[msg.Route] = msg.Signals;
+			} else {
+				Console.WriteLine("Initializing signals for route " + msg.Route);
+				lastSignals[msg.Route] = msg.Signals;
+			}
 		}
 
 		public void SetUnlockedSwitches(Messages.UnlockedSwitchesMessage msg) {
-			Console.WriteLine("< SetUnlockedSwitches(" + msg + ")");
 		}
 
 		public void UpdateTrainData(Messages.TrainDataMessage msg) {
-			Console.WriteLine("< UpdateTrainData(" + msg + ")");
+		}
+
+		private static void UpdateListOfIntegers(Dictionary<int, List<int>> lastDict, int route, List<int> items, string objectName, string addedText, string removedText) {
+			items.Sort();
+			if (lastDict.ContainsKey(route)) {
+				List<int> last = lastDict[route];
+				int i = 0, j = 0;
+				while ((i < last.Count) || (j < items.Count)) {
+					if ((i < last.Count) && (j < items.Count) && (last[i] == items[j])) {
+						// Equal
+						++i;
+						++j;
+					} else if ((i < last.Count) && ((j == items.Count) || (last[i] < items[j]))) {
+						// element i is less than element j
+						Console.WriteLine("Route " + route + " " + objectName + " number " + last[i] + " " + removedText + ".");
+						++i;
+					} else {
+						// element j is less than element i
+						Console.WriteLine("Route " + route + " " + objectName + " number " + items[j] + " " + addedText + ".");
+						++j;
+					}
+				}
+				lastDict[route] = items;
+			} else {
+				Console.WriteLine("Initializing " + objectName + " table for route " + route);
+				lastDict[route] = items;
+			}
 		}
 	}
 }
