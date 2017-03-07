d1786295e986df36a17c6c3a4a5f00fca449acfa
Christopher Head <chead@chead.ca>
2017-03-06 23:53:45 -0800

    Add AbstractDispatcher utility class.
    This class can be extended by a subclass when that subclass needs to
    implement IDispatcher, doesn't extend any other base class, and only
    cares about a few of the possible messages.

---
+++ iecc8/AbstractDispatcher.cs
@@ -0,0 +1,44 @@
+﻿using Iecc8.Messages;
+
+namespace Iecc8 {
+	/// <summary>
+	/// An implementation of IDispatcher that doesn't do anything.
+	/// </summary>
+	public abstract class AbstractDispatcher : IDispatcher {
+		public virtual void DTMF(DTMFMessage pMessage) {
+		}
+
+		public virtual void PermissionUpdate(DispatcherPermissionMessage pMessage) {
+		}
+
+		public virtual void Ping() {
+		}
+
+		public virtual void RadioText(RadioTextMessage pMessage) {
+		}
+
+		public virtual void SendSimulationState(SimulationStateMessage pMessage) {
+		}
+
+		public virtual void SetInterlockErrorSwitches(InterlockErrorSwitchesMessage pMessage) {
+		}
+
+		public virtual void SetOccupiedBlocks(OccupiedBlocksMessage pMessage) {
+		}
+
+		public virtual void SetOccupiedSwitches(OccupiedSwitchesMessage pMessage) {
+		}
+
+		public virtual void SetReversedSwitches(ReversedSwitchesMessage pMessage) {
+		}
+
+		public virtual void SetSignals(SignalsMessage pMessage) {
+		}
+
+		public virtual void SetUnlockedSwitches(UnlockedSwitchesMessage pMessage) {
+		}
+
+		public virtual void UpdateTrainData(TrainDataMessage pMessage) {
+		}
+	}
+}
