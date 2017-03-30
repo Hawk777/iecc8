8c6913f91108655d127e9f03d6a9dbb6cb196302
Christopher Head <chead@chead.ca>
2017-03-29 23:12:53 -0700

    WIP train details.

---
+++ iecc8/UI/TrainDetail.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Iecc8.UI {
+	/// <summary>
+	/// Interaction logic for TrainDetail.xaml
+	/// </summary>
+	public partial class TrainDetail : UserControl {
+		public TrainDetail() {
+			InitializeComponent();
+		}
+	}
+}
