e61e6b28def771f9ecd1b5873a6386a2fc2c5f16
Christopher Head <chead@chead.ca>
2017-04-08 13:12:45 -0700

    Allow manipulating points and signals.

--- iecc8/Program.cs
+++ iecc8/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.ServiceModel;
+﻿using Iecc8.Messages;
+using System;
+using System.ServiceModel;
 using System.Threading.Tasks;
 
 namespace Iecc8 {
@@ -16,8 +18,34 @@ namespace Iecc8 {
 			using (DuplexChannelFactory<IRun8> factory = new DuplexChannelFactory<IRun8>(ic, binding, ADDRESS)) {
 				Run8Wrapper obj = new Run8Wrapper(factory.CreateChannel());
 				await obj.DispatcherConnectedAsync();
+				int defaultSubArea = 0;
 				for (;;) {
-					System.Threading.Thread.Sleep(5000);
+					string line = Console.ReadLine();
+					if (line.Length >= 3) {
+						char objectType = line[0];
+						bool hasAction = objectType != 'S';
+						char action = hasAction ? line[line.Length - 1] : '\0';
+						int target;
+						bool targetOK = int.TryParse(line.Substring(1, line.Length - (hasAction ? 2 : 1)), out target);
+						if (targetOK && (objectType == 'S')) {
+							defaultSubArea = target;
+						} else if (targetOK && (objectType == 's') && ((action == 's') || (action == 'p') || (action == 'f') || (action == 'F'))) {
+							await obj.ChangeSignalAsync(defaultSubArea, target, (action == 's') ? ESignalIndication.Stop : (action == 'p') ? ESignalIndication.Proceed : (action == 'f') ? ESignalIndication.Fleet : ESignalIndication.FlagBy);
+						} else if (targetOK && (objectType == 'p') && ((action == 'n') || (action == 'r') || (action == 'u') || (action == 'l'))) {
+							await obj.ThrowSwitchAsync(defaultSubArea, target, (action == 'n') ? ESwitchState.Normal : (action == 'r') ? ESwitchState.Reversed : (action == 'u') ? ESwitchState.Unlocked : ESwitchState.Locked);
+						} else {
+							Console.WriteLine("Usage:");
+							Console.WriteLine("S123 - set subsequent commands to target sub-area 123");
+							Console.WriteLine("p123n - throw points 123 to normal");
+							Console.WriteLine("p123r - throw points 123 to reverse");
+							Console.WriteLine("p123u - unlock points 123 for hand cranking");
+							Console.WriteLine("p123l - lock points 123 for power use");
+							Console.WriteLine("s123s - set signal 123 to stop");
+							Console.WriteLine("s123p - set signal 123 to proceed");
+							Console.WriteLine("s123f - set signal 123 to fleet");
+							Console.WriteLine("s123F - set signal 123 to flag-by");
+						}
+					}
 				}
 			}
 		}
