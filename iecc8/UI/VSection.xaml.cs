51ef24215f21a43804f22df426e38fa96e3561be
Christopher Head <chead@chead.ca>
2017-04-14 19:48:22 -0700

    Add vertical track section control.

---
+++ iecc8/UI/VSection.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace Iecc8.UI {
+	/// <summary>
+	/// Interaction logic for VSection.xaml
+	/// </summary>
+	public partial class VSection : TrackSection {
+		/// <summary>
+		/// Constructs a new VSection.
+		/// </summary>
+		public VSection() {
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Refreshes the appearance of this object.
+		/// </summary>
+		protected override void Update() {
+			Rectangle.Fill = RenderColour;
+		}
+	}
+}
