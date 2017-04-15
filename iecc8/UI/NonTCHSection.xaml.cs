131df058d3c295e9cbe43a501b0a935a123518dd
Christopher Head <chead@chead.ca>
2017-04-15 01:39:34 -0700

    Add some non-circuited track pieces.

---
+++ iecc8/UI/NonTCHSection.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Iecc8.UI {
+	/// <summary>
+	/// Interaction logic for NonTCHSection.xaml
+	/// </summary>
+	public partial class NonTCHSection : UserControl {
+		/// <summary>
+		/// Whether there is a TCB at the left end of this section.
+		/// </summary>
+		public bool TCBLeft {
+			get {
+				return (bool) GetValue(TCBLeftProperty);
+			}
+			set {
+				SetValue(TCBLeftProperty, value);
+			}
+		}
+
+		/// <summary>
+		/// The TCB at left end property.
+		/// </summary>
+		public static readonly DependencyProperty TCBLeftProperty = DependencyProperty.Register(nameof(TCBLeft), typeof(bool), typeof(NonTCHSection));
+
+		/// <summary>
+		/// Whether there is a TCB at the right end of this section.
+		/// </summary>
+		public bool TCBRight {
+			get {
+				return (bool) GetValue(TCBRightProperty);
+			}
+			set {
+				SetValue(TCBRightProperty, value);
+			}
+		}
+
+		/// <summary>
+		/// The TCB at right end property.
+		/// </summary>
+		public static readonly DependencyProperty TCBRightProperty = DependencyProperty.Register(nameof(TCBRight), typeof(bool), typeof(NonTCHSection));
+
+		/// <summary>
+		/// Constructs a new NonTCHSection.
+		/// </summary>
+		public NonTCHSection() {
+			InitializeComponent();
+		}
+	}
+}
